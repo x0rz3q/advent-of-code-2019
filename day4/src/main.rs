2c1e9b16272487fe7605232ee66152b309b2c26e
J.M. Thiessen <jacob@x0rz3q.com>
2019-12-07 21:25:05 +0100

    Lint the code of day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,54 +1,54 @@
 fn is_increasing(number: usize) -> bool {
-    let mut number = number;
-    let mut last = usize::max_value();
+	let mut number = number;
+	let mut last = usize::max_value();
 
-    while number > 0 {
-        let digit = number % 10;
-        if last < digit {
-            return false;
-        }
+	while number > 0 {
+		let digit = number % 10;
+		if last < digit {
+			return false;
+		}
 
-        last = digit;
-        number = number / 10;
-    }
+		last = digit;
+		number = number / 10;
+	}
 
-    return true;
+	return true;
 }
 
 fn has_pattern(number: usize, min: usize, max: usize) -> bool {
-    let mut count = vec![0,0,0,0,0,0,0,0,0,0,0];
-    let mut number = number;
+	let mut count = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+	let mut number = number;
 
-    while number > 0 {
-        let digit = number % 10;
-        count[digit] += 1;
+	while number > 0 {
+		let digit = number % 10;
+		count[digit] += 1;
 
-        number = number / 10;
-    }
+		number = number / 10;
+	}
 
-    for i in count {
-        if i >= min && i <= max{
-            return true;
-        }
-    }
+	for i in count {
+		if i >= min && i <= max {
+			return true;
+		}
+	}
 
-    false
+	false
 }
 
 fn main() {
-    let mut count = 0;
-    for i in 134564..585159 + 1 {
-        if is_increasing(i) && has_pattern(i, 2, 10) {
-            count += 1;
-        }
-    }
-    println!("Silver: {}", count);
+	let mut count = 0;
+	for i in 134564..585159 + 1 {
+		if is_increasing(i) && has_pattern(i, 2, 10) {
+			count += 1;
+		}
+	}
+	println!("Silver: {}", count);
 
-    let mut count = 0;
-    for i in 134564..585159 + 1 {
-        if is_increasing(i) && has_pattern(i, 2, 2) {
-            count += 1;
-        }
-    }
-    println!("Gold: {}", count);
+	let mut count = 0;
+	for i in 134564..585159 + 1 {
+		if is_increasing(i) && has_pattern(i, 2, 2) {
+			count += 1;
+		}
+	}
+	println!("Gold: {}", count);
 }
