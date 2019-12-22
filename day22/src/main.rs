d5b904ecb77bbc19e37b05d2af180b86602abc15
J.M. Thiessen <jacob@x0rz3q.com>
2019-12-22 18:04:57 +0100

    Re-implement part2, with a cleaner and better explained solution

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,12 +1,44 @@
 extern crate num_bigint;
 extern crate num_traits;
 
+use num::integer::div_floor;
+use num::integer::mod_floor;
 use num_bigint::BigInt;
+use std::ops::Add;
 use std::ops::Mul;
 use std::ops::Sub;
-use std::ops::Add;
-use num::integer::mod_floor;
-use num::integer::div_floor;
+
+#[derive(Clone, Debug)]
+struct Function {
+	a: BigInt,
+	b: BigInt,
+	modulus: BigInt,
+}
+
+impl Function {
+	fn new(a: BigInt, b: BigInt, modulus: BigInt) -> Function {
+		Function { a, b, modulus }
+	}
+
+	fn compose(&self, other: Function) -> Function {
+		let mut z = self.a.clone().mul(other.a.clone());
+		z = mod_floor(z, self.modulus.clone());
+		let mut q = other.b.mul(self.a.clone());
+		q = q.add(self.b.clone());
+		q = mod_floor(q, self.modulus.clone());
+
+		Function::new(z, q, self.modulus.clone())
+	}
+
+	fn solve(&self, x: u64) -> BigInt {
+		let mut result = self.a.clone();
+		result = result.mul(x);
+		result = result.add(self.b.clone());
+		result = mod_floor(result, self.modulus.clone());
+
+		result
+	}
+}
 
 fn cut(cards: Vec<i64>, n: i64) -> Vec<i64> {
 	let mut n = n;
@@ -45,7 +77,7 @@ fn step(cards: Vec<i64>, rules: Vec<String>) -> Vec<i64> {
 
 	for rule in rules {
 		if rule.contains("deal into new stack") {
-			cards = deal_new_deck(cards);	
+			cards = deal_new_deck(cards);
 		} else if rule.contains("cut") {
 			let parts: Vec<String> = rule.split(" ").map(|x| x.to_string()).collect();
 			let offset = parts[1].to_string().parse::<i64>().unwrap();
@@ -94,23 +126,44 @@ fn inverse(a: BigInt, m: BigInt) -> BigInt {
 	x
 }
 
-fn matrix_multiplication() {
+fn construct_function(f: Function, repeat: u64) -> Function {
+	if repeat == 0 {
+		return Function {
+			a: BigInt::from(1),
+			b: BigInt::from(0),
+			modulus: f.modulus.clone(),
+		};
+	}
+
+	let is_odd = repeat % 2 == 1;
+	let middle = repeat / 2;
+
+	let g = construct_function(f.clone(), middle);
+	let gog = g.compose(g.clone());
+
+	if !is_odd {
+		return gog;
+	}
+
+	gog.compose(f.clone())
 }
 
 /**
  * We want to calculate this with algebra, otherwise the running time would be crazy.
- * If we start thinking about what happens we get the following, we have a multiplier and an
- * offset, that gets changed in every step. Which means we could write this as an equation,
- * which means we have ax+b. Hence, we initialize a = 1 and b = 0 in the beginning. Simply
- * representing the index of each element. When we see a cut we want to offset all items by the
- * amount given, hence we get b = b + cut % modulus, when we see a deal into we need to flip the entire
- * structure, which means we need to flip the multiplier so a = a * -1, but we also need to make
- * sure our offset is still handled correctly so b = -b - 1, flipping it. When we see a deal with
- * increments we get that everything is increased by a certain number, so we get a = a * inverse(increment),
- * b = b * inverse(increment). Simply, multiplying everything with this. Everything is modulus the amount of
- * cards, to make sure we do not go out of bounds.
+ * We first observe that we have an offset and a multiplier, that gets changed by each "rule" of
+ * shuffle we need to follow. Which means, we can write this as an equation ax+b. We want to apply
+ * these rules backwards, because we are interested in the inverse. So, if we encounter a new we
+ * simply do b' = b*-1 - 1, a' = a * -1, if we encounter cut we simply have b' = b + offset, and if
+ * we encounter a increment we multiply a and b by the inverse of the given offset.
  *
- * We then want to find a * x + b = 2020 mod modulus.
+ * We then want to after repeating (ax+b) 101741582076661 times, we can solve this in multiple
+ * ways. We could do this with modular arithmetic or by repeating the inverse function
+ * 101741582076661 times. Personally, I tried to do it both ways as can be seen in the git history
+ * but I think the repeating inverse function results in cleaner code, that is easier to explain.
+ * After repeating this 101741582076661 times we simply need to fill out our x and we have what we
+ * need. Repeating in this context means the following, f(f(f(....))), basically using the compose
+ * rules e.g. fog(x). We then can solve it by f(f(f(...)))(2020), which will yield the correct
+ * result.
  */
 fn gold(rules: Vec<String>) -> BigInt {
 	let modulus: u64 = 119315717514047;
@@ -127,9 +180,6 @@ fn gold(rules: Vec<String>) -> BigInt {
 			let parts: Vec<String> = rule.split(" ").map(|x| x.to_string()).collect();
 			let offset = parts[1].to_string().parse::<i64>().unwrap();
 			b = b.add(offset);
-			if offset < 0 {
-				b = b.add(modulus);
-			}
 		} else {
 			let parts: Vec<String> = rule.split(" ").map(|x| x.to_string()).collect();
 			let offset = parts[3].to_string().parse::<i64>().unwrap();
@@ -138,40 +188,34 @@ fn gold(rules: Vec<String>) -> BigInt {
 			a = a.mul(inverse.clone());
 			b = b.mul(inverse);
 		}
-
-		a = mod_floor(a, BigInt::from(modulus));
-		b = mod_floor(b, BigInt::from(modulus));
 	}
 
 	a = mod_floor(a, BigInt::from(modulus));
 	b = mod_floor(b, BigInt::from(modulus));
+	let f = Function {
+		a: a.clone(),
+		b: b.clone(),
+		modulus: BigInt::from(modulus),
+	};
 
-	let mut theta = a.modpow(&BigInt::from(repeat), &BigInt::from(modulus));
-	theta = theta.mul(2020);
-	theta = mod_floor(theta, BigInt::from(modulus));
-
-	let mut phi = a.modpow(&BigInt::from(repeat), &BigInt::from(modulus));
-	phi = phi.add(modulus - 1);
-	phi = mod_floor(phi, BigInt::from(modulus));
-
-	let mut lambda = b.mul(phi);
-	lambda = mod_floor(lambda, BigInt::from(modulus));
-
-	let mut omega: BigInt = a.sub(1);
-	omega = inverse(omega.clone(), BigInt::from(modulus));
-	
-	let mut result = lambda.mul(omega);
-	result = result.add(theta);
-	result = mod_floor(result, BigInt::from(modulus));
-
-	result
+	let composition = construct_function(f, repeat);
+	composition.solve(2020)
 }
 
 fn main() {
-	assert_eq!(cut(vec![0,1,2,3,4,5,6,7,8,9], 3), vec![3,4,5,6,7,8,9,0,1,2]);
-	assert_eq!(cut(vec![0,1,2,3,4,5,6,7,8,9], -4), vec![6,7,8,9,0,1,2,3,4,5]);
-	assert_eq!(deal_new_deck(vec![0,1,2,3]), vec![3,2,1,0]);
-	assert_eq!(deal_increment(vec![0,1,2,3,4,5,6,7,8,9], 3), vec![0,7,4,1,8,5,2,9,6,3]);
+	assert_eq!(
+		cut(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 3),
+		vec![3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
+	);
+	assert_eq!(
+		cut(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9], -4),
+		vec![6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
+	);
+	assert_eq!(deal_new_deck(vec![0, 1, 2, 3]), vec![3, 2, 1, 0]);
+	assert_eq!(
+		deal_increment(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 3),
+		vec![0, 7, 4, 1, 8, 5, 2, 9, 6, 3]
+	);
 
 	let rules: Vec<String> = include_str!("input")
 		.trim()
