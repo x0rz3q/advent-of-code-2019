d8cfcf8e2c9b0a4e900d7e73b9f818cf97afc4aa
J.M. Thiessen <jacob@x0rz3q.com>
2019-12-26 12:38:24 +0100

    Lint source code of day06

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -3,77 +3,77 @@ use std::collections::HashSet;
 use std::collections::VecDeque;
 
 fn count(mapping: &HashMap<String, Vec<String>>, target: String) -> u64 {
-    let mut c: u64 = 0;
+	let mut c: u64 = 0;
 
-    if !mapping.contains_key(&target) {
-        return 0;
-    }
+	if !mapping.contains_key(&target) {
+		return 0;
+	}
 
-    let sources = mapping.get(&target).unwrap().to_vec();
-    c += sources.len() as u64;
+	let sources = mapping.get(&target).unwrap().to_vec();
+	c += sources.len() as u64;
 
-    for source in sources {
-        c += count(mapping, source);
-    }
+	for source in sources {
+		c += count(mapping, source);
+	}
 
-    c
+	c
 }
 
 /// Just a recursion
 fn silver(input: Vec<String>) -> u64 {
-    let mut mapping: HashMap<String, Vec<String>> = HashMap::new();
-    for i in input {
-        let split: Vec<String> = i.split(')').map(|item| String::from(item)).collect();
-        let from = split[0].to_string();
-        let to = split[1].to_string();
+	let mut mapping: HashMap<String, Vec<String>> = HashMap::new();
+	for i in input {
+		let split: Vec<String> = i.split(')').map(|item| String::from(item)).collect();
+		let from = split[0].to_string();
+		let to = split[1].to_string();
 
-        if mapping.contains_key(&to) {
-            let mut orbits: Vec<String> = mapping.get(&to).unwrap().to_vec();
-            orbits.push(from.to_string());
-        } else {
-            let mut orbits: Vec<String> = Vec::new();
-            orbits.push(from.to_string());
-            mapping.insert(to.to_string(), orbits);
-        }
-    }
+		if mapping.contains_key(&to) {
+			let mut orbits: Vec<String> = mapping.get(&to).unwrap().to_vec();
+			orbits.push(from.to_string());
+		} else {
+			let mut orbits: Vec<String> = Vec::new();
+			orbits.push(from.to_string());
+			mapping.insert(to.to_string(), orbits);
+		}
+	}
 
-    let mut c = 0;
-    for (source, _targets) in mapping.clone() {
-        c += count(&mapping.clone(), source.to_string());
-    }
+	let mut c = 0;
+	for (source, _targets) in mapping.clone() {
+		c += count(&mapping.clone(), source.to_string());
+	}
 
-	c	
+	c
 }
 
 /// Simple BFS like solution
 fn gold(input: Vec<String>) -> u64 {
 	let mut mapping: HashMap<String, Vec<String>> = HashMap::new();
 
-    for i in input {
-        let split: Vec<String> = i.split(')').map(|item| String::from(item)).collect();
-        let from = split[0].to_string();
-        let to = split[1].to_string();
+	for i in input {
+		let split: Vec<String> = i.split(')').map(|item| String::from(item)).collect();
+		let from = split[0].to_string();
+		let to = split[1].to_string();
 
-        if mapping.contains_key(&to) {
-            let mut orbits: Vec<String> = mapping.get(&to).unwrap().to_vec();
-            orbits.push(from.to_string());
+		if mapping.contains_key(&to) {
+			let mut orbits: Vec<String> = mapping.get(&to).unwrap().to_vec();
+			orbits.push(from.to_string());
 			mapping.insert(to.to_string(), orbits);
-        } else {
-            let mut orbits: Vec<String> = Vec::new();
-            orbits.push(from.to_string());
-            mapping.insert(to.to_string(), orbits);
-        }
+		} else {
+			let mut orbits: Vec<String> = Vec::new();
+			orbits.push(from.to_string());
+			mapping.insert(to.to_string(), orbits);
+		}
 
-        if mapping.contains_key(&from) {
-            let mut orbits: Vec<String> = mapping.get(&from).unwrap().to_vec();
-            orbits.push(to.to_string());
+		if mapping.contains_key(&from) {
+			let mut orbits: Vec<String> = mapping.get(&from).unwrap().to_vec();
+			orbits.push(to.to_string());
 			mapping.insert(from.to_string(), orbits);
-        } else {
-            let mut orbits: Vec<String> = Vec::new();
-            orbits.push(to.to_string());
-            mapping.insert(from.to_string(), orbits);
-        }
-    }
+		} else {
+			let mut orbits: Vec<String> = Vec::new();
+			orbits.push(to.to_string());
+			mapping.insert(from.to_string(), orbits);
+		}
+	}
 
 	/* BFS */
 	let mut queue = VecDeque::new();
@@ -104,12 +104,12 @@ fn gold(input: Vec<String>) -> u64 {
 }
 
 fn main() {
-    let input: Vec<String> = include_str!("input")
-        .trim()
-        .split('\n')
-        .map(|row| String::from(row))
-        .collect();
+	let input: Vec<String> = include_str!("input")
+		.trim()
+		.split('\n')
+		.map(|row| String::from(row))
+		.collect();
 
-    println!("Silver: {}", silver(input.clone()));
+	println!("Silver: {}", silver(input.clone()));
 	println!("Gold: {}", gold(input.clone()));
 }
